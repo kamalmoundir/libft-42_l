12c4c43d0165eef4240526163f5ec20033ca251c
kamalmoundir <moundirkamal@gmail.com>
2024-09-15 18:21:40 +0200

    add strdup

---
+++ ft_strdup.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kmoundir <kmoundir@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/15 17:59:06 by kmoundir          #+#    #+#             */
+/*   Updated: 2024/09/15 18:21:02 by kmoundir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strdup(const char *s)
+{
+	
+	char	*str;
+	char	*tmp;
+	size_t	len;
+
+	len = ft_strlen(s);
+	str=(char *)malloc((len +1) * sizeof(char));
+	if(!str)
+		return (NULL);
+	tmp = str;
+	while(*s)
+	{
+		*(tmp ++) = *(s ++);
+	}
+	*tmp = '\0';
+	return (str);
+}
+
